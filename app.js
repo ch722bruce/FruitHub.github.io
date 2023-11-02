f502ef2b4bc1ed773a67d86a5814de2326670c7b
ch722bruce <brucehuang722@gmail.com>
2023-11-01 23:45:21 -0700

    setup vite react framework

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
+
+import indexRouter from "./routes/index.js";
+
+
+// ES6 modules don't have __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "front", "dist")));
+
+app.use("/", indexRouter);
+
+export default app;
