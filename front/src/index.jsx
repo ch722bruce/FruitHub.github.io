525004b74ee474d185a79405e6d66a627a860b35
ch722bruce <brucehuang722@gmail.com>
2023-11-02 15:36:39 -0700

    update index.jsx

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const root = document.getElementById('root');
-const reactRoot = ReactDOM.createRoot(root);
-
-reactRoot.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
