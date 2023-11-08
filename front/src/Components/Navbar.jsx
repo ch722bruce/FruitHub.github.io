f2a2fa13010e2781921571b44ad5450225bd961f
ch722bruce <brucehuang722@gmail.com>
2023-11-07 22:20:44 -0800

    update signout

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 import PropTypes from "prop-types";
 import SignOut from "./SignOut";
 
-
 function Navbar() {
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    // This function should update the `user` state with the session stored user
+    const sessionUser = sessionStorage.getItem("userId"); // or any other user-related key
+    if (sessionUser) {
+      setUser({}); // Set user to an empty object or fetch user details
+    }
+  }, []);
 
   return (
     <div>
@@ -19,13 +26,14 @@ function Navbar() {
           </li>
           <li className="nav-item">
             <Link to="/checkout" className="brand nav-link">
-              Carts {" "}
+              Carts{" "}
             </Link>
           </li>
         </ul>
         <div className="navbar-nav signout">
-          <SignOut />
-          </div>
+          {/* Render the SignOut component only if `user` is not null */}
+          {user && <SignOut />}
+        </div>
       </nav>
     </div>
   );
