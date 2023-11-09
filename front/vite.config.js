f6055b905c99c47ceba774b2deeabde43e1f1696
Zhehao <x97589125@gmail.com>
2023-11-08 17:29:04 -0800

    specify port number by env

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.PORT,
     proxy: {
       '/api': {
         target: "http://localhost:3000",
