7dbf1595a1a9d4e521e1260d4141b09eb1b51956
ch722bruce <brucehuang722@gmail.com>
2023-12-12 13:53:34 -0800

    color palette

--- front/src/Components/CheckOut.jsx
+++ front/src/Components/CheckOut.jsx
@@ -1,28 +1,29 @@
 import Title from "./Title";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import QuantityBtn from "./QuantityBtn";
 import { CartContext } from "./CartContext";
-import { useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export default function Checkout() {
   let { cartItems } = useContext(CartContext);
   let cartEmpty = cartItems.length <= 0;
-  let grandTotal = cartItems.reduce((total, product) => {
-    return (total + product.price.slice(1) * product.quantity);
-  }, 0).toFixed(2);
+  let grandTotal = cartItems
+    .reduce((total, product) => {
+      return total + product.price.slice(1) * product.quantity;
+    }, 0)
+    .toFixed(2);
   const freeShippingPrice = 99;
   const navigate = useNavigate();
   useEffect(() => {
-    if(!sessionStorage.getItem("userId")){
+    if (!sessionStorage.getItem("userId")) {
       navigate("/");
     }
-   }, [navigate]);
-
+  }, [navigate]);
 
   return (
     <>
-      <Title mainTitle="Your Carts"/>
+      <Title mainTitle="Your Carts" />
       {cartEmpty && (
         <div className="container">
           <div className="nothingInCart">
@@ -44,23 +45,32 @@ export default function Checkout() {
                   <tr key={product._id}>
                     <td>
                       <Link to={"/product/" + product._id}>
-                        <img
-                          src={product.image_url}
-                          alt={product.name}
-                        />
+                        <img src={product.image_url} alt={product.name} />
                       </Link>
                     </td>
                     <td>
-                      <p>Name : {product.name}</p>
-                      <p>Price : {product.price}</p>
-                      <p>Description : {product.description}</p>
+                      <p>
+                        <strong>Name:</strong> {product.name}
+                      </p>
+                      <p>
+                        <strong>Price:</strong> {product.price}
+                      </p>
+                      <p>
+                        <strong>Description:</strong>{" "}
+                        {product.description.split(" ").slice(0, 10).join(" ")}
+                        {product.description.split(" ").length > 10
+                          ? "..."
+                          : ""}
+                      </p>
                     </td>
+
                     <td width="200">
                       <QuantityBtn productInfo={product} />
                     </td>
                     <td>
                       <div className="productSubTotal">
-                        ${(product.price.slice(1) * product.quantity).toFixed(2)}
+                        $
+                        {(product.price.slice(1) * product.quantity).toFixed(2)}
                       </div>
                     </td>
                   </tr>
@@ -91,5 +101,5 @@ export default function Checkout() {
 
 Checkout.prototype = {
   isLogin: PropTypes.bool,
-  userLogout: PropTypes.func.isRequired
+  userLogout: PropTypes.func.isRequired,
 };
